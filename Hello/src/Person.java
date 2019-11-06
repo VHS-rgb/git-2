3856e19c1d158d07570380d150fb1b304478eca5
konstantinolehno <alekhna@mail.ru>
2019-11-06 17:14:57 +0300

    add hello

---
+++ Hello/src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void sayHelloToPerson(){
+        System.out.println("Hi" + getName());
+    }
+
+}
